4b7dade93c0575ccc8c640a3d563e1c18b493e49
LURKS02 <thecrooks2@gmail.com>
2023-03-20 07:12:52 +0900

    Initial Commit

---
+++ PokeBook/PokeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeBookApp.swift
+//  PokeBook
+//
+//  Created by 디해 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct PokeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
