c25e734c8960928a8977b3e27d6cfad622fc5dba
LURKS02 <thecrooks2@gmail.com>
2024-02-26 17:14:27 +0900

    포켓몬 종류 뷰

---
+++ PokeBook/Presentation/Component/SubTitle.swift
@@ -0,0 +1,26 @@
+//
+//  SubTitle.swift
+//  PokeBook
+//
+//  Created by 디해 on 2023/07/26.
+//
+
+import SwiftUI
+
+struct SubTitle: View {
+    let title: String
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(Color(hexString: "838383"))
+            Spacer()
+        }
+    }
+}
+
+struct SubTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        SubTitle(title: "부 타이틀")
+    }
+}
